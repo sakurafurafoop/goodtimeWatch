2ed23ee386fb91aa62ea81da8fe2655988912a5c
SAKU <saku.bel.5724@gmail.com>
2018-05-19 23:49:53 +0900

    Initial Commit

---
+++ SakuraWatchTests/SakuraWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  SakuraWatchTests.swift
+//  SakuraWatchTests
+//
+//  Created by SAKU on 2018/05/19.
+//  Copyright © 2018年 SAKU. All rights reserved.
+//
+
+import XCTest
+@testable import SakuraWatch
+
+class SakuraWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
